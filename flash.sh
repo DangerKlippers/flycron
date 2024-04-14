2674d5eb7768d707a1d19ca70a9ad6f2dfa14188
Bea Nance <bwnance@gmail.com>
2024-04-14 14:37:16 -0400

    flash update

--- flash.sh
+++ flash.sh
@@ -8,9 +8,9 @@ stty -F /dev/serial/by-id/usb-Armchair_Heavy_Industries_Flycron_44003E0001513331
 sleep 1
 sudo dfu-util -D /tmp/flycron-8.bin -a 0 -R -s 0x08000000:leave
 sleep 1
-stty -F /dev/serial/by-id/usb-Armchair_Heavy_Industries_Flycron_1F004E001351303239373335-if00 1200
+stty -F /dev/serial/by-id/usb-Armchair_Heavy_Industries_Flycron_1F0059000151333232333138-if00 1200
 sleep 1
-sudo dfu-util -D /tmp/flycron-25.bin -a 0 -R -s 0x08000000:leave
+sudo dfu-util -D /tmp/flycron-8.bin -a 0 -R -s 0x08000000:leave
 sleep 1
 stty -F /dev/serial/by-id/usb-Armchair_Heavy_Industries_Flycron_45006C000151333138363230-if00 1200
 sleep 1
